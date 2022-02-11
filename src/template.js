1b2e2cfa651828fc890f375d06ec2b9e0dd75ee4
kompasim <kompasim@163.com>
2022-02-12 00:47:39 +0800

    init

---
+++ src/template.js
@@ -0,0 +1,34 @@
+
+/**
+ * simple template renderer
+ * @param {*} html 
+ * @returns renderer func
+ */
+
+var template2renderer = function(html) {
+    var re = /<%([^%>]+)?%>/g;
+    var reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
+    var code = 'var r=[];\\n';
+    var cursor = 0
+    var match;
+    var add = function(line, js) {
+        if (js) {
+            code += line.match(reExp) ? line + '\\n' : 'r.push(' + line + ');\\n';
+        } else {
+            code += line != '' ? 'r.push(`' + line.replace(/"/g, '\\"') + '`);\\n' : '';
+        }
+        return add;
+    }
+    while(match = re.exec(html)) {
+        add(html.slice(cursor, match.index))(match[1], true);
+        cursor = match.index + match[0].length;
+    }
+    add(html.substr(cursor, html.length - cursor), false);
+    code += 'return r.join("");';
+    code = code.replace(/\\t+|\\r\\n+|\\n+/g, '');
+    var fun = new Function(code);
+    var renderer = function(args) {
+        return fun.apply(args);
+    }
+    return renderer;
+}
