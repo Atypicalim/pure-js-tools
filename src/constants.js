b70c1740bd39f330fa802a8cf217f6936489e8f0
kompasim <kompasim@ziipin.com>
2024-01-24 21:35:43 +0800

    update constant

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,20 @@
 
 
 let ALL_HTML_TAGA = [
-    'div', 'span',
-    'p', 'pre',
+    'head', 'title', 'base', 'link', 'meta', 'script', 'style',
+    'div',
+    'span',
+    'p',
     'br', 'hr',
     'b', 'strong', 'i', 'em', 'mark', 'small', 'del', 'ins', 'sub', 'sup',
+    'code', 'kbd', 'samp', 'var', 'pre',
     'abbr', 'address', 'bdo', 'blockquote', 'cite', 'q',
     'a',
     'button',
     'img', 'map', 'area', 'picture',
+    'audio', 'video', 'source', 'track',
+    'canvas',
+    'svg',
     'iframe',
     'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
     'ul', 'ol', 'li', 'dl', 'dt', 'dd',
